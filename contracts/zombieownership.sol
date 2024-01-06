2ca1ef50b9a57e086c6a79f524b6eb131b088ab1
Siddhant Kulkarni <kulkarni.sid0809@gmail.com>
2024-01-07 00:25:30 +0530

    ERC721 contract imported and inherited

--- contracts/zombieownership.sol
+++ contracts/zombieownership.sol
@@ -2,6 +2,8 @@ pragma solidity >=0.5.0 <0.6.0;
 
 import "./zombieattack.sol";
 
-contract ZombieOwnership is ZombieAttack {
+import "./erc721.sol";
+
+contract ZombieOwnership is ZombieAttack , ERC721 {
 
 }
