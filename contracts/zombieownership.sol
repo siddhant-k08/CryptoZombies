4b1ee7f5133edaa3b13837668c9d530ec5431d94
Siddhant Kulkarni <kulkarni.sid0809@gmail.com>
2024-01-08 14:33:01 +0530

    Functionality added

--- contracts/zombieownership.sol
+++ contracts/zombieownership.sol
@@ -5,6 +5,8 @@ import "./erc721.sol";
 
 contract ZombieOwnership is ZombieAttack, ERC721 {
 
+  mapping (uint => address) zombieApprovals;
+
   function balanceOf(address _owner) external view returns (uint256) {
     return ownerZombieCount[_owner];
   }
@@ -13,7 +15,7 @@ contract ZombieOwnership is ZombieAttack, ERC721 {
     return zombieToOwner[_tokenId];
   }
 
-  function _transfer(address _from, address _to, uint256 _tokenId) private{
+  function _transfer(address _from, address _to, uint256 _tokenId) private {
     ownerZombieCount[_to]++;
     ownerZombieCount[_from]--;
     zombieToOwner[_tokenId] = _to;
@@ -21,10 +23,12 @@ contract ZombieOwnership is ZombieAttack, ERC721 {
   }
 
   function transferFrom(address _from, address _to, uint256 _tokenId) external payable {
-
+    require(zombieToOwner[_tokenId] == msg.sender || zombieApprovals[_tokenId] == msg.sender);
+    _transfer(_from, _to , _tokenId);
   }
 
   function approve(address _approved, uint256 _tokenId) external payable {
 
   }
+
 }
