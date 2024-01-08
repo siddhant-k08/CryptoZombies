4b071f5fc563ecb69cc2946a600cd7307d18b53d
Siddhant Kulkarni <kulkarni.sid0809@gmail.com>
2024-01-08 14:48:43 +0530

    Event emission

--- contracts/zombieownership.sol
+++ contracts/zombieownership.sol
@@ -27,9 +27,9 @@ contract ZombieOwnership is ZombieAttack, ERC721 {
     _transfer(_from, _to, _tokenId);
   }
 
-
-  function approve(address _approved, uint256 _tokenId) external payable onlyOwnerOf(_tokenId){
+  function approve(address _approved, uint256 _tokenId) external payable onlyOwnerOf(_tokenId) {
     zombieApprovals[_tokenId] = _approved;
+    emit Approval(msg.sender, _approved, _tokenId);
   }
 
 
